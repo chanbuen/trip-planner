b49dc584aa4aecc47eded10a22e77da4f77d99cd
Abigail Demsas <ademsas1026@gmail.com>
2018-03-20 15:43:04 -0400

    third commit

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ const mapboxgl = require("mapbox-gl");
 mapboxgl.accessToken = "pk.eyJ1IjoiY2hhbmJ1ZW4iLCJhIjoiY2plenRxbGFsMGgyaDMzbzB5OHp6bW9oOCJ9.Rq1jdrn4wcmIZ9A529czSA";
 
 const map = new mapboxgl.Map({
-  container: "map",
-  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
-  zoom: 12, // starting zoom
-  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+    container: "map",
+    center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+    zoom: 12, // starting zoom
+    style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
 });
 
 let markerDomEl = document.createElement('div');
@@ -17,4 +17,3 @@ markerDomEl.style.height = "39px";
 markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)"
 
 new mapboxgl.Marker(markerDomEl).setLngLat([-74.009151, 40.705086]).addTo(map);
-
